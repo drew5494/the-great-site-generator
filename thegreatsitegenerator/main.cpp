87ff668cb13ebb6c40451035daadc957de4cf210
drew5494 <tassoone@gmail.com>
2021-10-14 21:19:09 -0400

    Refactoring main.cpp to improve code maintainability:
    *Extract readFolder() function
    *Refactor main to re-use readFile()
    *Refactor readFolder() and readFile() into Reading
    *Split main.cpp into reading.h and main.cpp

--- thegreatsitegenerator/main.cpp
+++ thegreatsitegenerator/main.cpp
@@ -4,21 +4,13 @@
 //
 //  Created by Andrew Tassone on 2021-09-14.
 //
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <filesystem>
 #include "TGSGConfig.h"
+#include "reading.h"
 
 using namespace std;
-bool hasLang;
-const char* lang;
-void readFile(string fname);
-void readMarkdownFile(string fname);
 
 int main(int argc, const char** argv) {
+    Reading r;
     if (argv[1] == "-h"sv || argv[1] == "--help"sv){
         cout << "Please type -i followed by the name of the the text file or folder.\n";
         cout << "Please type -m followed by the name of the the markdown file or folder.\n";
@@ -49,18 +41,11 @@ int main(int argc, const char** argv) {
             //Convert text file
             if (lastindex != string::npos) {
                 cout << "Converting: " << filename << endl;
-                readFile(filename);
-            } else {
-                //Convert folder
-                for (const auto& dir : std::__fs::filesystem::recursive_directory_iterator(filename)) {
-                    string path = dir.path().string();
-                    size_t text = path.find(".txt");
-
-                    if (text != string::npos) {
-                        cout << "Converting: " << path << endl;
-                        readFile(path);
+                    r.readFile(filename, 1);
                     }
-                }
+            else
+            {
+                r.readFolder(filename, 1);
             }
             cout << "\033[1mALL DONE!\033[0m\nYour GREAT html has been placed in the dist folder.\nHave a wonderful day :)\n";
         }
@@ -81,18 +66,11 @@ int main(int argc, const char** argv) {
             //Convert text file
             if (lastindex != string::npos) {
                 cout << "Converting: " << filename << endl;
-                readMarkdownFile(filename);
-            } else {
-                //Convert folder
-                for (const auto& dir : std::__fs::filesystem::recursive_directory_iterator(filename)) {
-                    string path = dir.path().string();
-                    size_t markdown = path.find(".md");
-                    
-                    if (markdown != string::npos) {
-                        cout << "Converting markdown: " << path << endl;
-                        readMarkdownFile(path);
+                r.readFile(filename, 2);
                     }
-                }
+            else
+            {
+                r.readFolder(filename, 2);
             }
             cout << "\033[1mALL DONE!\033[0m\nYour GREAT html has been placed in the dist folder.\nHave a wonderful day :)\n";
         }
@@ -106,82 +84,4 @@ int main(int argc, const char** argv) {
         return 1;
     }
     return 0;
-}
-void readFile(string fname) {
-    std::ifstream file (fname.c_str());
-    filesystem::path dir (fname);
-    fname = dir.filename();
-    size_t lastindex = fname.find_last_of(".");
-    string rawname = fname.substr(0, lastindex);
-    // Create output file.
-    std::ofstream htmlFile;
-    htmlFile.open ("dist/"+rawname+".html");
-    // write the header
-    htmlFile << "<!doctype html>" << '\n'<< "<html lang=";
-    //Choose language       
-    if (!hasLang){
-        htmlFile  << "\"en-ca\">" << '\n';
-    }
-    else {
-        htmlFile  << "\"" << lang << "\">" << '\n';
-    }
-    htmlFile << "<head>" << '\n' << "<meta charset=\"utf-8\">" << "\n" << "<title>" << rawname << "</title>" << '\n' << "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">" << '\n' << "</head>" << '\n' << "<body>" << '\n';
-    // Write the body
-    //Getting Title
-    string line1, line2, line3;
-    getline(file, line1);
-    getline(file, line2);
-    getline(file, line3);
-
-    if (line1 != "" && line2 == "" && line3 == "")
-    {
-        string title = line1;
-        htmlFile << "<h1>" << title << "</h1>" << '\n';
-    }
-    else
-    {
-        htmlFile << "<p>" << line1 << "</p>" << '\n';
-        htmlFile << "<p>" << line2 << "</p>" << '\n';
-        htmlFile << "<p>" << line3 << "</p>" << '\n';
-    }
-    // read a line
-    std::string line{};
-    while (std::getline(file, line)) {
-        // Write line with <p> tag
-        if(!line.size())
-            htmlFile << '\n';
-        else
-            htmlFile << "<p>" << line << "</p>" << '\n';
-    }
-    // End of body
-    htmlFile << "</body>" << '\n' << "</html>" << '\n';
-}
-
-void readMarkdownFile(string fname) {
-    std::ifstream file (fname.c_str());
-    fname = fname.substr(fname.find_last_of("/\\") + 1);
-    size_t lastindex = fname.find_last_of(".");
-    string rawname = fname.substr(0, lastindex);
-    // Create output file.
-    std::ofstream htmlFile;
-    htmlFile.open ("dist/"+rawname+".html");
-    // write the header
-    htmlFile << "<!doctype html>" << '\n' << "<html lang=\"en\">" << '\n' << "<head>" << '\n' << "<meta charset=\"utf-8\">" << "\n" << "<title>" << rawname << "</title>" << '\n' << "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">" << '\n' << "</head>" << '\n' << "<body>" << '\n';
-    // Write the body
-    // read a line
-    std::string line{};
-
-    while (std::getline(file, line)) {
-        // Write line with <p> tag
-        if(!line.size())
-            htmlFile << '\n';
-        else if(line[0] == '#'){
-            line.erase (0,2);
-            htmlFile << "<h1>" << line << "</h1>" << '\n';
-        }
-        else
-            htmlFile << "<p>" << line << "</p>" << '\n';
-    }
-    // End of body
-    htmlFile << "</body>" << '\n' << "</html>" << '\n';
 }
