d2ce6eea082830dfacaf10f3ed9008b91d64937a
drew5494 <tassoone@gmail.com>
2021-11-05 23:21:48 -0400

    Add clang-format

---
+++ CONTRIBUTING.md
@@ -0,0 +1,14 @@
+<h2>Tool Setup</h2>
+<p>This builds TGSG with cmake so you can run it using the <code>./tgsg</code>
+command.</p>
+<ul>
+    <li>Clone the repository</li>
+    <li><code>cd</code> into the folder</li>
+    <li>run <code>cmake .</code> to generate a Makefile</li>
+    <li>run <code>make</code></li>
+</ul>
+<h2>Committing Guidelines</h2>
+<p>Before committing any changes, be sure to follow these steps.</p>
+<ul>
+    <li>Ensure you have <code>clang-format</code> installed in VSCode</li>
+</ul>
--- thegreatsitegenerator/main.cpp
+++ thegreatsitegenerator/main.cpp
@@ -9,78 +9,78 @@
 
 using namespace std;
 
-int main(int argc, const char** argv) {
+int main(int argc, const char** argv)
+{
     Reading r;
-    if (argv[1] == "-h"sv || argv[1] == "--help"sv){
-        cout << "Please type -i followed by the name of the the text file or folder.\n";
-        cout << "Please type -m followed by the name of the the markdown file or folder.\n";
-    }
-    else if (argv[1] == "-v"sv || argv[1] == "--version"sv){
-        cout << "Version " << TGSG_VERSION_MAJOR << "." << TGSG_VERSION_MINOR << endl;
-    }
-    else if (argv[1] == "-i"sv || argv[1] == "--input"sv){
-        if (filesystem::exists(argv[2])){
-            if (argv[3]){
-            if (argv[3] == "-l"sv || argv[3] == "--lang"sv){
-                if (argv[4] && (argv[4] == "en-ca"sv || argv[4] == "pt-br"sv || argv[4] == "fr"sv)){
-                    lang = argv[4];
-                    hasLang = true;
+    if (argv[1] == "-h"sv || argv[1] == "--help"sv) {
+        cout << "Please type -i followed by the name of the the text file or "
+                "folder.\n";
+        cout << "Please type -m followed by the name of the the markdown file or "
+                "folder.\n";
+    } else if (argv[1] == "-v"sv || argv[1] == "--version"sv) {
+        cout << "Version " << TGSG_VERSION_MAJOR << "." << TGSG_VERSION_MINOR
+             << endl;
+    } else if (argv[1] == "-i"sv || argv[1] == "--input"sv) {
+        if (filesystem::exists(argv[2])) {
+            if (argv[3]) {
+                if (argv[3] == "-l"sv || argv[3] == "--lang"sv) {
+                    if (argv[4] && (argv[4] == "en-ca"sv || argv[4] == "pt-br"sv || argv[4] == "fr"sv)) {
+                        lang = argv[4];
+                        hasLang = true;
+                    } else {
+                        cerr
+                            << "Please enter a valid language. Type -h for more details.\n";
+                        return 1;
+                    }
                 } else {
-                    cerr << "Please enter a valid language. Type -h for more details.\n"; 
+                    cerr << "Please enter a valid argument. Type -h for more details.\n";
                     return 1;
                 }
-            } else {
-                cerr << "Please enter a valid argument. Type -h for more details.\n"; 
-                return 1;
-            }
             }
             string filename = argv[2];
             size_t lastindex = filename.find_last_of(".");
-            //Create new directory
+            // Create new directory
             filesystem::create_directory("dist");
-            //Convert text file
+            // Convert text file
             if (lastindex != string::npos) {
                 cout << "Converting: " << filename << endl;
-                    r.readFile(filename, 1);
-                    }
-            else
-            {
+                r.readFile(filename, 1);
+            } else {
                 r.readFolder(filename, 1);
             }
-            cout << "\033[1mALL DONE!\033[0m\nYour GREAT html has been placed in the dist folder.\nHave a wonderful day :)\n";
-        }
-        else {
-            cerr << "File or folder does not exist. Type -h for more details.\n"; //Identify user
+            cout << "\033[1mALL DONE!\033[0m\nYour GREAT html has been placed in the "
+                    "dist folder.\nHave a wonderful day :)\n";
+        } else {
+            cerr
+                << "File or folder does not exist. Type -h for more details.\n"; // Identify
+                                                                                 // user
             return 1;
         }
-    }
-    else if (argv[1] == "-m"sv || argv[1] == "--markdown"sv){
-        if (argv[2]){
+    } else if (argv[1] == "-m"sv || argv[1] == "--markdown"sv) {
+        if (argv[2]) {
             string filename = argv[2];
             size_t lastindex = filename.find_last_of(".");
-            //Create new directory
+            // Create new directory
             if (mkdir("dist", 0777) == -1)
                 cerr << "Error: " << strerror(errno) << endl;
             else
                 cout << "Directory created...\n";
-            //Convert text file
+            // Convert text file
             if (lastindex != string::npos) {
                 cout << "Converting: " << filename << endl;
                 r.readFile(filename, 2);
-                    }
-            else
-            {
+            } else {
                 r.readFolder(filename, 2);
             }
-            cout << "\033[1mALL DONE!\033[0m\nYour GREAT html has been placed in the dist folder.\nHave a wonderful day :)\n";
-        }
-        else {
-            cerr << "Please enter a valid file name. Type -h for more details.\n"; //Identify user
+            cout << "\033[1mALL DONE!\033[0m\nYour GREAT html has been placed in the "
+                    "dist folder.\nHave a wonderful day :)\n";
+        } else {
+            cerr
+                << "Please enter a valid file name. Type -h for more details.\n"; // Identify user
             return 1;
         }
-    }
-    else {
-        cerr << "Type -h for more details.\n"; //Identify user
+    } else {
+        cerr << "Type -h for more details.\n"; // Identify user
         return 1;
     }
     return 0;
